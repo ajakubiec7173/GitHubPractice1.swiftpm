1ae5a1ec6b8d7cb30599bc75fb93f4f6f0b2e84f
Adam <ajakubiec7173@stu.d214.org>
2024-04-10 14:35:48 -0500

    Made textfield and connected it to variable number2

--- ContentView.swift
+++ ContentView.swift
@@ -6,11 +6,11 @@ struct ContentView: View {
     @State var number2: Int
     
     var body: some View {
-        VStack {
+        TextField("Enter a Number", value: $number2, format: .number)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
     }
-}
+
