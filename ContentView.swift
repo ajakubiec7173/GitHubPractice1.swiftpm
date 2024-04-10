375bff339aad8171fde5f7524e361bab7a7b1148
Adam <ajakubiec7173@stu.d214.org>
2024-04-10 14:51:49 -0500

    Added var and text

--- ContentView.swift
+++ ContentView.swift
@@ -5,9 +5,11 @@ struct ContentView: View {
     
     @State var number2: Int
     
+    @State var answer: Int
     var body: some View {
         TextField("Enter a number",value: $number1,format: .number)
         TextField("Enter a Number", value: $number2, format: .number)
+        Text("\(answer)")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
